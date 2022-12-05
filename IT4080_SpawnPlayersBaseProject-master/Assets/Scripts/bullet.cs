14a2b294b29ec34333e9c291c9ef132d18c76456
Grant <grantchapel22@gmail.com>
2022-12-05 11:40:31 -0500

    e

--- IT4080_SpawnPlayersBaseProject-master/Assets/Scripts/bullet.cs
+++
@@ -1,9 +0,0 @@
-using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-using Unity.Netcode;
-
-public class bullet : NetworkBehaviour
-{
-    public NetworkVariable<int> Damage = new NetworkVariable<int>(2);  
-}
--- IT4080_SpawnPlayersBaseProject-master/README.md
+++
@@ -1,6 +0,0 @@
-# IT4080_PlayerMovementAndColors
-
-This is the implementation of Module 3's assignment.  This starts with the Base Networking Project and adds:
-* Networked player movement
-* Networked player colors
-
